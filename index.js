4668db853555bce97e8b804b7928245b60ff637c
Abdoo-270 <abdoo27073@gmail.com>
2023-09-27 11:08:56 +0300

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require("dotenv").config();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(cors());
 app.use(express.static("public"));
