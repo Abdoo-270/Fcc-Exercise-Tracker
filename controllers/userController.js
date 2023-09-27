1061f05ed60dfd8329c5beecbc8e7ca235f213b8
Abdoo-270 <abdoo27073@gmail.com>
2023-09-27 11:15:44 +0300

    -

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,10 +6,6 @@ const createUser = async (req, res) => {
   if (!username) {
     res.json({ error: "please provide username" });
   }
-  const userAlreadyExists = await User.findOne({ username });
-  if (userAlreadyExists) {
-    res.json({ error: "User already exists" });
-  }
   try {
     const user = await User.create({ username });
     res.status(StatusCodes.CREATED).json({ user });
