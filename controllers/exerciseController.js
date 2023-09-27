ef6916a412b0ff605d86c3a444d0f9305de94ebf
Abdoo-270 <abdoo27073@gmail.com>
2023-09-27 15:16:04 +0300

    -

--- controllers/exerciseController.js
+++ controllers/exerciseController.js
@@ -2,24 +2,25 @@ const Exercise = require("../models/Exercise");
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 
+//The response returned from POST /api/users/:_id/exercises will be the user object with the exercise fields added.
 const createExercise = async (req, res) => {
   const userId = req.params.id;
-  const user = await User.findOne({ _id: userId });
-  if (!user) {
+  const existingUser = await User.findOne({ _id: userId });
+  if (!existingUser) {
     res.status(StatusCodes.NOT_FOUND).json({ error: "no user found" });
   }
   const { description, duration, date } = req.body;
   if (!description || !duration) {
     res.status(StatusCodes.BAD_REQUEST).json("please fill all the fields");
   }
-  const exercise = await Exercise.create({
-    _id: user[0]._id,
-    username: user[0].username,
-    date,
-    duration,
+  const exercise = new Exercise({
     description,
+    duration,
+    date: date || new Date(),
+    user: existingUser._id,
   });
-
+  await exercise.save();
+  console.log(exercise);
   res.status(StatusCodes.CREATED).json(exercise);
 };
 
--- models/Exercise.js
+++ models/Exercise.js
@@ -1,24 +1,10 @@
 const mongoose = require("mongoose");
 const User = require("./User");
 const ExerciseSchema = new mongoose.Schema({
-  username: {
-    type: String,
-  },
-  _id: {
-    type: String,
-  },
-  description: {
-    type: String,
-    required: [true, "Please provide description"],
-  },
-  duration: {
-    type: Number,
-    required: [true, "please provide duraiton"],
-  },
-  date: {
-    type: Date,
-    default: new Date(),
-  },
+  description: String,
+  duration: Number,
+  date: Date,
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 });
 
 module.exports = mongoose.model("Exercise", ExerciseSchema);
