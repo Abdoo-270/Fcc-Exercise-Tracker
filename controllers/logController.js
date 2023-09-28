68b65d3598a1156df36f77285e11c4ea34346f38
Abdoo-270 <abdoo27073@gmail.com>
2023-09-28 14:19:10 +0300

    -

--- controllers/logController.js
+++ controllers/logController.js
@@ -4,13 +4,23 @@ const Log = require("../models/Log");
 const { StatusCodes } = require("http-status-codes");
 
 const getSingleUserLogs = async (req, res) => {
+  const { from, to, limit } = req.query; // Parse query parameters
+
   const userId = req.params.id;
   const user = await User.find({ _id: userId });
   const username = user[0].username;
+  let query = { username };
+  // Apply date filters if 'from' and 'to' are provided
+  if (from && to) {
+    query.date = { $gte: new Date(from), $lte: new Date(to) };
+  }
+  const userExercises = await Exercise.find(query)
+    .select({
+      username: 0,
+    })
+    .limit(parseInt(limit) || undefined) // Limit the number of logs
+    .sort({ date: "desc" }); // Sort logs by date in descending order
 
-  const userExercises = await Exercise.find({ username }).select({
-    username: 0,
-  });
   const log = await Log.create({
     username,
     count: userExercises.length,
